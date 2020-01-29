286c9c0f90b8e1d4d92b00416b780f07742f50c9
Craig Gumbley <craiggumbley@gmail.com>
2020-01-29 13:49:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Azure Redis Exporter
+
+An MVS to DUMP and RESTORE a redis database to another instance in Azure.
+
+* Enter source and destination values in `config/default.json`
+* The port and db settings are common across both clients
+
+```
+npm install
+node exporter.js
+
+```
---
+++ exporter.js
@@ -0,0 +1,32 @@
+const redis = require('redis');
+const dump = require('redis-dump-restore').dump;
+const cliProgress = require('cli-progress');
+const config = require('config');
+
+const port = config.get('redis.port');
+const db = config.get('redis.db');
+const source = config.get('redis.source');
+const dest = config.get('redis.destination');
+
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+const source_client = redis.createClient(port, source.host,
+    { auth_pass: source.key, tls: { servername: source.host }, detect_buffers: true, db: db});
+
+const dest_client = redis.createClient(port, dest.host,
+    { auth_pass: dest.key, tls: { servername: dest.host }, detect_buffers: true, db: db});
+
+const d = dump(source_client, '*');
+
+bar.start(100, 0);
+d.on('data', function (key, data, ttl) {
+    bar.increment()
+    dest_client.restore(key, ttl, data);
+}).on('error', function (err) {
+    console.error(`An error occured while processing: ${err}`)
+}).on('end', function () {
+    bar.stop();
+    source_client.quit();
+    dest_client.quit();
+    console.log('Migration Complete!')
+});
