cd56cb272f55ad09c3d3b3cf2c2c4c87762e72f9
Craig Gumbley <craiggumbley@gmail.com>
2020-01-29 15:50:00 +0000

    style and linting improvements

--- exporter.js
+++ exporter.js
@@ -1,5 +1,5 @@
 const redis = require('redis');
-const dump = require('redis-dump-restore').dump;
+const redisDump = require('redis-dump-restore').dump;
 const cliProgress = require('cli-progress');
 const config = require('config');
 
@@ -10,37 +10,49 @@ const dest = config.get('redis.destination');
 
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-const source_client = redis.createClient(port, source.host,
-    { auth_pass: source.key, tls: { servername: source.host }, detect_buffers: true, db: db});
+const sourceClient = redis.createClient(port, source.host,
+  {
+    auth_pass: source.key,
+    tls: {
+      servername: source.host,
+    },
+    detect_buffers: true,
+    db,
+  });
 
-const dest_client = redis.createClient(port, dest.host,
-    { auth_pass: dest.key, tls: { servername: dest.host }, detect_buffers: true, db: db});
+const destClient = redis.createClient(port, dest.host,
+  {
+    auth_pass: dest.key,
+    tls: {
+      servername: dest.host,
+    },
+    detect_buffers: true,
+    db,
+  });
 
-const d = dump(source_client, '*');
+const dump = redisDump(sourceClient, '*');
 
-source_client.dbsize( function(err, size) {
-    if (err) {
-        console.error(`An error occured in the calback: ${err}`)
-        return;
-    }
+sourceClient.dbsize((err, size) => {
+  if (err) {
+    console.error(`An error occured in the calback: ${err}`);
+    return;
+  }
 
-    bar.start(size, 0);
+  bar.start(size, 0);
 
-    d.on('data', function (key, data, ttl) {
-        bar.increment()
-        dest_client.restore(key, ttl, data, function(err) {
-            if (err) {
-                throw Error(`An error occured while restoring a key: ${err}`);
-            }
-        });
-    }).on('error', function (err) {
-        console.error(`An error occured while processing: ${err}`)
-    }).on('end', function () {
-        bar.stop();
-        source_client.quit();
-        dest_client.quit();
-        console.log('Migration Complete!')
+  dump.on('data', (key, data, ttl) => {
+    bar.increment();
+    destClient.restore(key, ttl, data, (restoreErr) => {
+      if (err) {
+        throw Error(`An error occured while restoring a key: ${restoreErr}`);
+      }
     });
+  }).on('error', (dumpErr) => {
+    console.error(`An error occured while processing: ${dumpErr}`);
+  }).on('end', () => {
+    bar.stop();
+    sourceClient.quit();
+    destClient.quit();
+    console.log('Migration Complete!');
+  });
 });
-
-
